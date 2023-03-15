7f10d6724ec3226de2b5280a2d3d13ed5ef191a3
Flutterappuser <programeur2017@gmail.com>
2023-03-15 01:25:01 +0100

    add the ability to delete a tesk

--- lib/task_list_screen.dart
+++ lib/task_list_screen.dart
@@ -20,13 +20,65 @@ class TaskListScreen extends StatelessWidget {
             itemCount: tasks.length,
             itemBuilder: (context, index) {
               final task = tasks[index];
-              return CheckboxListTile(
-                title: Text(task.title),
-                subtitle: Text(task.description),
-                value: task.isDone,
-                onChanged: (value) {
-                  viewModel.toggleTask(task);
+              return Dismissible(
+                key: Key(task.id.toString()),
+                direction: DismissDirection.horizontal,
+                background: Container(
+                  color: Colors.red,
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Icon(Icons.delete, color: Colors.white),
+                ),
+                secondaryBackground: Container(
+                  color: Colors.red,
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Icon(Icons.delete, color: Colors.white),
+                ),
+                confirmDismiss: (direction) async {
+                  if (direction == DismissDirection.endToStart ||
+                      direction == DismissDirection.startToEnd) {
+                    // Show a dialog box to confirm the deletion.
+                    bool confirmDelete = await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("Delete Task"),
+                          content: Text(
+                              "Are you sure you want to delete this task?"),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              child: Text("Cancel"),
+                              onPressed: () {
+                                Navigator.of(context).pop(false);
+                              },
+                            ),
+                            ElevatedButton(
+                              child: Text("Delete"),
+                              onPressed: () {
+                                Navigator.of(context).pop(true);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    return confirmDelete;
+                  }
+                  return false;
                 },
+                onDismissed: (direction) {
+                  // Call a function to delete the task.
+                  viewModel.removeTask(task);
+                },
+                child: CheckboxListTile(
+                  title: Text(task.title),
+                  subtitle: Text(task.description),
+                  value: task.isDone,
+                  onChanged: (value) {
+                    viewModel.toggleTask(task);
+                  },
+                ),
               );
             },
           );
@@ -81,7 +133,7 @@ class TaskListScreen extends StatelessWidget {
                 final title = titleController.text.trim();
                 final description = descriptionController.text.trim();
 
-                if (title.isNotEmpty && description.isNotEmpty) {
+                if (title.isNotEmpty) {
                   final task = Task(
                     id: _uuid.v4(),
                     title: title,
